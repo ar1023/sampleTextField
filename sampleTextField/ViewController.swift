2a7ff458fe488f4f162f409d11b5a795903885e8
ar1023 <ar1023.hz@gmail.com>
2016-06-01 11:07:13 +0800

    Initial Commit

---
+++ sampleTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleTextField
+//
+//  Created by 土屋陽香 on 2016/06/01.
+//  Copyright © 2016年 Haruka Tsuchiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
